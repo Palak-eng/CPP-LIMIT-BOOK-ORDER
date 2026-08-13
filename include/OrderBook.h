842e42c1b9e8250130ac2882fc6ada5c3f04e48a
Palak Barsaiyan <palakbarsaiyan@gmail.com>
2026-08-13 22:49:21 +0530

    add orderbooks

---
+++ include/OrderBook.h
@@ -0,0 +1,26 @@
+#ifndef ORDERBOOK_H
+#define ORDERBOOK_H
+
+#include <map>
+#include <vector>
+#include "Order.h"
+
+
+class OrderBook {
+    private:
+        std ::map<int, std::deque<Order>, std ::greater<int>> buyBook;
+        std::map<int, std ::deque<Order>> SellBook;
+
+    public:
+         void addOrder(const Order& order);
+};
+
+#endif
+
+
+
+
+
+
+
+
